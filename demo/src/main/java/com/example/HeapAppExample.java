3a7b60269b47748ab7e9a114f9ca26971377d088
Prem Agarwal <prem.m.agarwal@gmail.com>
2022-05-09 19:47:59 -0700

    maeven project and adding given code

---
+++ demo/src/main/java/com/example/HeapAppExample.java
@@ -0,0 +1,32 @@
+package com.heap;
+
+import java.util.*;
+
+public class HeapAppExample {
+
+  public static void main(String[] args) {
+    // create a min heap
+    PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+
+    // add random value nodes
+    final Integer min = 300;
+    final Integer max = 1500;
+    final Integer n = 110;
+    System.out.printf("Race times in minutes:\n");
+    for (Integer i = 0; i < n; i++) {
+      // create a value
+      Integer val = min + (int) Math.floor(Math.random() * (max - min));
+      System.out.printf("%d ", val);
+      // student needs to write code to
+      // put the random value (val) into the heap
+    }
+    System.out.printf("\n");
+    System.out.printf("\n");
+    // student needs to write code to
+    // print the heap, without changing the heap
+
+    // student needs to write code to
+    // print the 5 smallest values in the heap
+    // the values can be removed from the heap (the heap can be changed)
+  }
+}
