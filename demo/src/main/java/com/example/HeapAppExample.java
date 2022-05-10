42adfef3ec0a9c47427e550dc0b2bafc909a09cd
Prem Agarwal <prem.m.agarwal@gmail.com>
2022-05-09 20:09:36 -0700

    implemented for loop to print in order and print best 5 speeds

--- demo/src/main/java/com/example/HeapAppExample.java
+++ demo/src/main/java/com/example/HeapAppExample.java
@@ -1,4 +1,4 @@
-package com.heap;
+package com.example;
 
 import java.util.*;
 
@@ -19,14 +19,24 @@ public class HeapAppExample {
       System.out.printf("%d ", val);
       // student needs to write code to
       // put the random value (val) into the heap
+      minHeap.add(val);
     }
     System.out.printf("\n");
     System.out.printf("\n");
     // student needs to write code to
     // print the heap, without changing the heap
-
+    System.out.println("Printing the heap:");
+    for (Integer num : minHeap) {
+      System.out.print(num + " ");
+    }
+    System.out.println("");
+    System.out.println("");
     // student needs to write code to
     // print the 5 smallest values in the heap
     // the values can be removed from the heap (the heap can be changed)
+    System.out.println("Printing the top 5 race times:");
+    for (int i = 0; i <= 4; i++) {
+      System.out.printf("#%d    %d%n", i, minHeap.remove());
+    }
   }
 }
